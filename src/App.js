62ee8d6d4891f13df912277ea36bd494da1801d7
Ahmad Tijjani Muhammad <atm9465@gmail.com>
2020-08-28 20:00:11 +0100

    Added Hotel and HoteList files

--- src/App.js
+++ src/App.js
@@ -3,8 +3,7 @@ import "./styles.css";
 
 export default function App() {
   return (
-    <div className="App">
-      <h1>Hello Me</h1>
+    <div className="App">      
       <h2>Start editing to see some magic happen!</h2>
     </div>
   );
